b965c768003164348ec9be000bd623e3b509b4fc
Artem Verbat <artem.verbat@gmail.com>
2017-03-07 13:40:40 +0200

    Initial commit. Created base structure.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Boilerplate for quick start landing-site building
+
+### Examples of projects based on it
+
+- *https://github.com/verbart/business-landing*
+- *https://github.com/verbart/spectral*
+- *https://github.com/verbart/strata*
+
+---
+
+### Contains
+
+- **Tools**
+  - `Webpack + Babel` - for build scripts
+  - `Pug (Jade)` - for preprocessing views
+  - `Stylus` - for preprocessing styles
+  - `PostCSS Autoprefixer` - for adding prefixes to css properties
+  - `Tinypng` - for optimize images
+  - `BrowserSync` - dev-server for live-coding
+  - `Gulp 4` - for build it
+- **Libs**
+  - `Fontawesome` - awesome icons pack
+  - `Twitter Bootstrap` - for layout grid only
+
+---
+
+### Developing
+
+1. Run `npm install` to install dependencies.
+
+2. Run `gulp` or `npm start` to start the development server.
+   It should automatically open the client in your browser when ready.
+
+### Build
+
+- Run `npm run build` for building.
---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+const gulp = require('gulp');
+const buffer = require('vinyl-buffer');
+const postcss = require('gulp-postcss');
+const stylus = require('gulp-stylus');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const del = require('del');
+const gutil = require('gulp-util');
+const tinypng = require('gulp-tinypng-nokey');
+const spritesmith = require('gulp.spritesmith');
+const webpack = require('webpack');
+const panini = require('panini');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+gulp.task('views', function () {
+  panini.refresh();
+
+  return gulp.src('./src/views/pages/**/*.html')
+    .pipe(panini({
+      root: './src/views/pages',
+      layouts: './src/views/layouts',
+      partials: './src/views/partials',
+      helpers: './src/helpers',
+      data: './src/data'
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./src/styles/app.styl')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(stylus({
+      'include css': true
+    })
+    .on('error', function(error) {
+      gutil.log(gutil.colors.red('Error: ' + error.message));
+      this.emit('end');
+    }))
+    .pipe(gulpIf(!isDevelopment, postcss([
+      autoprefixer({
+        browsers: ['> 5%', 'ff > 14']
+      })
+    ])))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulpIf(!isDevelopment, cleanCSS()))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('scripts', function(done) {
+  return webpack(require('./webpack.config.js'), function(err, stats) {
+    if(err) throw new gutil.PluginError('webpack', err);
+
+    gutil.log('[scripts]', stats.toString({
+      colors: gutil.colors.supportsColor,
+      chunks: false,
+      hash: false,
+      version: false
+    }));
+
+    done();
+  });
+});
+
+gulp.task('sprite', function() {
+  const spriteData = gulp.src('./src/images/sprite/*.*')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'images.styl',
+      algorithm: 'binary-tree',
+      padding: 2,
+      cssTemplate: './src/styles/sprites/sprite-template.mustache'
+    }));
+
+  spriteData.img
+    .pipe(buffer())
+    .pipe(gulpIf(!isDevelopment, tinypng()))
+    .pipe(gulp.dest('./public/images'));
+
+  spriteData.css.pipe(gulp.dest('./src/styles/sprites'));
+
+  return spriteData;
+});
+
+gulp.task('images', function () {
+  return gulp.src(['./src/assets/images/**/*.*', '!./src/assets/images/sprite/*.*'])
+    .pipe(gulpIf(!isDevelopment, tinypng()))
+    .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('./src/assets/fonts/**/*.*')
+    .pipe(rename(path => path.dirname = ''))
+    .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/views/**/*.html', gulp.series('views'));
+  gulp.watch('./src/styles/**/*.{css,styl}', gulp.series('styles'));
+  gulp.watch('./src/scripts/**/*.js', gulp.series('scripts'));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    // proxy: 'example.com',
+    // files: 'public/**/*.*',
+    // https: true,
+    server: './public',
+    port: 8080
+  });
+
+  browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('clean', function () {
+  return del('./public')
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+  'sprite',
+  gulp.parallel(
+    'views',
+    'styles',
+    'scripts',
+    'fonts',
+    'images'
+  )));
+
+gulp.task('default', gulp.series(
+  'build',
+  gulp.parallel(
+    'watch',
+    'serve'
+  )));
