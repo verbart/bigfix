4d99e3b0e295c8d23abbee7e85ec084c911b735a
Artem Verbat <artem.verbat@gmail.com>
2017-03-12 20:15:01 +0200

    Added block testimonials for each page.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
+import 'slick-carousel';
 
 $('.toggleButton').click(function () {
   $($(this).data('target')).toggleClass('isOpen');
 });
+
+$('.testimonials__slider').slick();
