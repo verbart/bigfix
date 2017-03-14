1cc9a89ba7ae351c0c45bf4622b8ca04a407912f
Artem Verbat <artem.verbat@gmail.com>
2017-03-14 20:19:19 +0200

    Added content to news page.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,11 @@
-import $ from 'jquery';
+const $ = window.$ = window.jQuery = require('jquery');
 import 'slick-carousel';
+import 'simplelightbox';
 
 $('.toggleButton').click(function () {
   $($(this).data('target')).toggleClass('isOpen');
 });
 
 $('.testimonials__slider').slick();
+
+$('.simplelightbox a').simpleLightbox();
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,42 @@ const webpack = require('webpack');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: './src/scripts/app.js',
-    output: {
-        path: './public/js',
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            options: { presets: ['es2015'] },
-            exclude: [/node_modules/]
-        }]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            IS_DEVELOPMENT: JSON.stringify(isDevelopment)
-        })
-    ]
+  entry: './src/scripts/app.js',
+  output: {
+    path: './public/js',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: { presets: ['es2015'] },
+      exclude: [/node_modules/]
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT: JSON.stringify(isDevelopment)
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.jQuery': 'jquery'
+    })
+  ]
 };
 
 if (!isDevelopment) {
-    module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            beautify: false,
-            mangle: false,
-            compress: true,
-            comments: false,
-            drop_console: true,
-            warnings: false,
-            unsafe: true
-        })
-    );
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: false,
+      compress: true,
+      comments: false,
+      drop_console: true,
+      warnings: false,
+      unsafe: true
+    })
+  );
 }
